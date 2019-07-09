09e2809d83c27acced7757737dc3bf619b8921c1
toohong5 <toohong5@gmail.com>
2019-07-09 12:11:32 +0900

    second commit

--- 00_startcamp/02_day/01_naver_search.py
+++ 00_startcamp/02_day/01_naver_search.py
@@ -12,7 +12,8 @@ soup = BeautifulSoup(html, 'html.parser') #정제(타입변경 : )
 #하나의 순위의 경로 가져와서 li(list) 뒤에 지우기(바뀌는거 찾기!), select 는 여러개 선택한다
 s=soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k')
 
-
 #리스트로 출력!
 for i in s:
     print(i.text) #키워드만 출력
+
+# 두번째 커밋을 위한 주석!!!
