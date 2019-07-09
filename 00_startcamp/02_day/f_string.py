0f1780b87d0f7bb2b5308d309f89a2f39a1726cb
toohong5 <toohong5@gmail.com>
2019-07-09 15:47:15 +0900

    add .gitignore

---
+++ 00_startcamp/02_day/f_string.py
@@ -0,0 +1,25 @@
+#f-string로 문자열 출력
+
+# name = '박홍은'
+# print(f'안녕하세요, {name} 입니다.') #변수만 변경하면 됨
+
+
+# 점심 메뉴 추천
+
+import random
+
+menu = ['부대찌개', '소고기', '카레']
+lunch = random.choice(menu)
+
+print(f'오늘의 점심은 {lunch} 입니다.')
+
+
+# 로또 추천
+numbers = range(1,46)
+lotto = random.sample(numbers, 6)
+
+print(f'오늘의 로또 당첨 번호는 {sorted(lotto)} 입니다.')
+
+# 필요하면 이렇게도 해보자
+name = '홍길동'
+print('안녕하세요, '+ name +' 입니다.')
---
+++ 00_startcamp/02_day/string_interpolation.py
@@ -0,0 +1,10 @@
+#과거
+'%s %s' % ('one', 'two')
+
+#pyformat
+'{} {}'.format('one', 'two')
+
+#f-string(new in 3.6) _ 권장!!
+a='one'
+b='two'
+print(f'[{a}, {b}]')
